dd9c7a845dc37951fdbd1312ff9b6f3414b83a0e
ukiran03 <ushakiranreddi@gmail.com>
2026-03-07 11:41:03 +0530

    feat: end of implementation, failed design
    starting the rewrite

--- trash.go
+++ trash.go
@@ -9,6 +9,12 @@ type TrashImpl interface {
 	//   - if it is HomeTrash, hopefully os.Rename will be enough
 	//   - if it is SpecialTrash, copy+delete should be implemented
 	MoveToTrash() error
+
+	Put(src string) error
+	Restore(fe *FileEntry, dst string) error
+	Remove(fe *FileEntry) error // permanent deletion
+	List() ([]*FileEntry, error)
+	Info()
 }
 
 func (ht *HomeTrash) SourceDir() string {
