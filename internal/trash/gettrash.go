43af2f8af90563835c4175a116cc53d2f0a869b6
ukiran03 <ushakiranreddi@gmail.com>
2026-04-25 22:56:14 +0530

    refactor(trash): single function to get TrashPath

--- internal/trash/gettrash.go
+++ internal/trash/gettrash.go
@@ -5,42 +5,44 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+
+	"golang.org/x/sys/unix"
+	"ukiran.com/urm/internal/env"
 )
 
-type InitAction int
+// getTrashPath: returns the valid TrashPath for the given mountpoint(topDir),
+// for Home: return XDG_DATA_HOME/Trash
+// for Non-Home: M01 or M02
+func getTrashPath(topDir string, atHome bool) string {
+	if atHome {
+		trashHome := os.Getenv("XDG_DATA_HOME")
+		if trashHome == "" {
+			trashHome = filepath.Join(env.HomeDir, ".local", "share")
+		}
+		return filepath.Join(trashHome, "Trash")
+	}
 
-const (
-	DoInit InitAction = iota
-	NoInit            // Dry run
-)
-
-// getHomeTrashPath: returns the valid TrashPath from the current
-// user's HomeDir return error otherwise.
-func getHomeTrashPath(homePath string) (string, error) {
-	homeTrash := filepath.Join(homePath, ".local", "share", "Trash")
-	return ensureTrashDir(homeTrash)
-}
-
-// getSpecialTrashPath: returns the valid TrashPath for the given
-// Mountpoint, either M01 or M02 (creates if needed), return error
-// otherwise.
-func getSpecialTrashPath(rootPath string, uid int) (string, error) {
-	// Method (1): Admin created M01Trash: $topdir/.Trash/$uid
-	M01Trash := filepath.Join(rootPath, ".Trash", strconv.Itoa(uid))
-	exists, isSymlink, info, err := DirExists(M01Trash)
-
-	// NOTE: MAY also report the user
-	if exists && !isSymlink && err == nil {
-		sticky, _ := haveSticyBit(info, M01Trash)
-		permissioned, _ := havePermissions(info, M01Trash)
-		if sticky && permissioned {
-			return M01Trash, nil
+	// Method (1): $topdir/.Trash/$uid
+	// Spec requirement: $topdir/.Trash must be a directory (not a symlink) and
+	// must have the sticky bit set.
+	dotTrash := filepath.Join(topDir, ".Trash")
+	if info, err := os.Lstat(dotTrash); err == nil && info.IsDir() {
+		// check sticky bit
+		if (info.Mode() & os.ModeSticky) != 0 {
+			M01Trash := filepath.Join(dotTrash, strconv.Itoa(env.UID))
+			if info, err := os.Lstat(M01Trash); err == nil && info.IsDir() {
+				// check write permissions
+				if unix.Access(M01Trash, unix.W_OK|unix.X_OK) == nil {
+					return M01Trash
+				}
+			}
 		}
 	}
 
-	// Method (2): Per-User trash M02Trash: $topdir/.Trash-$uid
-	M02Trash := filepath.Join(rootPath, fmt.Sprintf(".Trash-%d", uid))
-	return ensureTrashDir(M02Trash)
+	// Method (2): Per-User trash $topdir/.Trash-$uid
+	// This is the fallback if Method 1 fails or .Trash doesn't exist.
+	M02Trash := filepath.Join(topDir, fmt.Sprintf(".Trash-%d", env.UID))
+	return M02Trash
 }
 
 // Helper to handle the directory creation logic
